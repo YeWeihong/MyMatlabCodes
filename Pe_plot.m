00cbfaff1bb5854f86000563493af61c88d5aff9
YeWeihong <weihong_ye@163.com>
2025-07-10 15:00:04 +0800

    修改

--- Pe_plot.m
+++ Pe_plot.m
@@ -5,7 +5,7 @@ pe_c2 = te2 .* ne2 * 2;
 [dp1, x_mid1] = calculate_derivative(te_rho1, pe_c1);
 [dp2, x_mid2] = calculate_derivative(te_rho2, pe_c2);
 
-figure()
+figure();
 plot(te_rho1, pe_c1)
 hold on
 plot(te_rho2, pe_c2)
